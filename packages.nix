9b3b1496185f215933fc001cf10ef5ba2d4be94d
myaroshi <user18487@proton.me>
2026-09-17 19:27:35 -0300

    Create packages.nix

---
+++ packages.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    # Terminal
+    alacritty
+
+    # Utilities
+    git
+    curl
+    wget
+    unzip
+    zip
+
+    # System tools
+    htop
+    btop
+    fastfetch
+
+    # File management
+    ranger
+
+    # Editors
+    neovim
+
+    # Wayland/X11 utilities
+    picom
+    xclip
+
+    # Networking
+    networkmanagerapplet
+  ];
+}
