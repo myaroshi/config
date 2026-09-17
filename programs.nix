a0e2d495cfc34a4ee36deb1906ae75fbe28a8790
myaroshi <user18487@proton.me>
2026-09-17 19:27:11 -0300

    Create programs.nix

---
+++ programs.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  # Window manager
+  services.xserver.windowManager.oxwm.enable = true;
+
+  # Display manager
+  services.xserver.displayManager.lightdm.enable = true;
+
+  # Shell
+  programs.zsh = {
+    enable = true;
+  };
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    noto-fonts
+    noto-fonts-emoji
+  ];
+
+  # Enable basic desktop services
+  services.dbus.enable = true;
+
+  # Sound
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+  };
+}
