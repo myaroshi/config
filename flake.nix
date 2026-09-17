ee9fbb7080b6f33889419a0651c4e26bba2e4073
myaroshi <user18487@proton.me>
2026-09-17 20:22:24 -0300

    Update home-manager username in flake.nix

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
-            home-manager.users.yourusername = import ./home/home.nix;
+            home-manager.users.lynn = import ./home.nix;
           }
         ];
       };
